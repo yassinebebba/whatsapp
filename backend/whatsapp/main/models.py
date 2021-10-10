32b60635e44e65ab4c8b3bc38ecad34e0c6fc7db
yassinebebba <yassinebebba2@gmail.com>
2021-10-10 21:48:37 +0100

    feat(django): message DB model

--- backend/whatsapp/main/models.py
+++ backend/whatsapp/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name='sender_set', on_delete=models.DO_NOTHING, null=False)
+    receiver = models.ForeignKey(User, related_name='receiver_set', on_delete=models.DO_NOTHING, null=False)
+    content = models.CharField(max_length=250, null=False)
+    creation_date = models.DateTimeField(default=timezone.now, null=False)
+    delivered = models.BooleanField(default=False, null=False)
+    read = models.BooleanField(default=False, null=False)
+    deleted = models.BooleanField(default=False, null=False)
+
+    def __repr__(self):
+        # return f'{self.sender.phone_number} - {self.receiver.phone_number}'
+        return f'{self.sender.useername} -> {self.receiver.useername}'
