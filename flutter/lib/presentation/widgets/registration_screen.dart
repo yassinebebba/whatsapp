eb4203d1fd8c0fe81e0bb69fc21fde81b848730c
i-zanis <ioanlafazanis@gmail.com>
2021-10-16 01:54:21 +0100

    feat(screens): input field phone verification

--- flutter/lib/presentation/widgets/registration_screen.dart
+++ flutter/lib/presentation/widgets/registration_screen.dart
@@ -5,27 +5,62 @@ import 'package:flutter/material.dart';
 class RegistrationScreen extends StatelessWidget {
   const RegistrationScreen({Key? key}) : super(key: key);
 
+  get controller => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-            child: Column(
-              children: <Widget>[
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const <Widget>[
-                      Text(""),
-                      Text(
-                        "Verify your phone number",
-                        style: TextStyle(
-                            fontSize: 18,
-                            color: greenColor,
-                            fontWeight: FontWeight.w500),
-                      ),
-                      Icon(Icons.more_vert)
-                    ])
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const <Widget>[
+                Text(""),
+                Text(
+                  "Verify your phone number",
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: greenColor,
+                      fontWeight: FontWeight.w500),
+                ),
+                Icon(Icons.more_vert)
               ],
-            )));
+            ),
+            const Text(
+              "WhatsApp Clone will send and SMS message (carrier charges may apply) to verify your phone number. Enter your country code and phone number:",
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            ListTile(
+              onTap: () => {},
+            ),
+            Row(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(
+                    color: greenColor,
+                  ))),
+                  child: Text(""),
+                ),
+                Expanded(
+                  child: Container(
+                    height: 40,
+                    child: TextField(
+                      controller: controller,
+                      decoration: InputDecoration(hintText: "Phone Number"),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
