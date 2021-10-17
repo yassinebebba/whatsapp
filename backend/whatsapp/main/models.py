488eef3ccedcd157abd88e7c0d5d76410f55c0fa
i-zanis <ioanlafazanis@gmail.com>
2021-10-17 05:55:22 +0100

    feat(django): activate()

--- backend/whatsapp/main/models.py
+++ backend/whatsapp/main/models.py
@@ -63,6 +63,11 @@ class CustomUser(AbstractBaseUser):
         except:
             return (None, False)
 
+    def activate(self) -> None:
+        self.is_active = True
+        self.save()
+        return None
+
     def has_perm(self, perm: str, obj=None) -> bool:
         if self.is_active and self.is_admin:
             return True
