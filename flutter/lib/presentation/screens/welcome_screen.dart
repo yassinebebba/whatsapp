b4310128a6d465f8cdd89cd27d07697d210a55bf
i-zanis <ioanlafazanis@gmail.com>
2021-10-26 01:16:15 +0100

    refactor(screens): WelcomeScreen add image|privacy policy message

--- flutter/lib/presentation/screens/welcome_screen.dart
+++ flutter/lib/presentation/screens/welcome_screen.dart
@@ -1,19 +1,68 @@
+import 'package:cross_platform/presentation/screens/registration_screen.dart';
 import 'package:cross_platform/presentation/widgets/theme/flutter_style.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-            child: Column(children: <Widget>[
-              Text("Welcome to WhatsApp",
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: primaryColor,
-                      fontWeight: FontWeight.bold))
-            ])));
+      body: Container(
+        margin: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 30,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              "Welcome to WhatsApp",
+              style: TextStyle(
+                fontSize: 20,
+                color: primaryColor,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Container(
+              height: 290,
+              width: 290,
+              child: Image.asset("assets/phone-background.png"),
+            ),
+            Column(
+              children: <Widget>[
+                Text(
+                  "Read our Privacy Policy, 'Agree and continue' to accept the Terms of Service",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 14),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                MaterialButton(
+                  color: greenColor,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => RegistrationScreen(),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    "AGREE AND CONTINUE",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
