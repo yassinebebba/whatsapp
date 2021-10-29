2189a4bbe3480d4affd674d062d46c1d6e339699
i-zanis <ioanlafazanis@gmail.com>
2021-10-29 02:29:47 +0100

    feat(screens): search bar

--- flutter/lib/presentation/screens/home_screen.dart
+++ flutter/lib/presentation/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cross_platform/presentation/widgets/theme/flutter_style.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,13 +10,60 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   bool _isSearch = false;
-  int _currentPageIndex = 1;
-  PageController _pageViewController = PageController(initialPage: 1);
 
-  List<Widget> get _pages => [];
+  _buildSearch() {
+    return Container(
+        height: 45,
+        margin: EdgeInsets.only(top: 25),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(.3),
+                spreadRadius: 1,
+                offset: Offset(0.0, 0.50))
+          ],
+        ),
+        child: TextField(
+            decoration: InputDecoration(
+                hintText: "Search...",
+                prefixIcon: InkWell(
+                  onTap: () {},
+                  child: Icon(Icons.arrow_back),
+                ))));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+            elevation: 0.0,
+            automaticallyImplyLeading: false,
+            backgroundColor:
+                _isSearch == true ? primaryColor : Colors.transparent,
+            title: _isSearch == false
+                ? Text("WhatsApp")
+                : Container(height: 0.0, width: 0.0),
+            flexibleSpace: _isSearch == false
+                ? Container(
+                    height: 0.0,
+                    width: 0.0,
+                  )
+                : _buildSearch(),
+            actions: <Widget>[
+              InkWell(
+                  onTap: () {
+                    setState(() {
+                      _isSearch = true;
+                    });
+                  },
+                  child: Icon(Icons.search)),
+              SizedBox(width: 5)
+            ]),
+        body: Column(
+          children: <Widget>[
+            TabBar(),
+          ],
+        ));
   }
 }
