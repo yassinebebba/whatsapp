06f56490f2526b46db13b6b982ba240744f6aa0d
i-zanis <ioanlafazanis@gmail.com>
2021-10-31 00:35:22 +0100

    feat(pages): SingleItemChatPage

---
+++ flutter/lib/presentation/pages/single_item_chat_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+
+class SingleItemChatPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      children: <Widget>[
+        Row(children: <Widget>[
+          Container(
+              height: 55,
+              width: 55,
+              child: ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                  child: Image.asset("assets/profile-default.png ")))
+        ])
+      ],
+    ));
+  }
+}
