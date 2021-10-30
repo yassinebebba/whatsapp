8a04132650e668f06cc8b9c7c1164d413a29592f
i-zanis <ioanlafazanis@gmail.com>
2021-10-30 03:27:13 +0100

    feat(widgets): TopTabBar

--- flutter/lib/presentation/widgets/top_tab_bar.dart
+++ flutter/lib/presentation/widgets/top_tab_bar.dart
@@ -1,40 +1,80 @@
 import 'package:cross_platform/presentation/widgets/theme/flutter_style.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class TopTabBar extends StatelessWidget {
+  final int index;
+
+  TopTabBar(this.index);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 50,
-      decoration: BoxDecoration(color: primaryColor),
+      decoration: const BoxDecoration(color: primaryColor),
+      child: Row(
+        children: <Widget>[
+          Container(
+            width: 40,
+            child: const Icon(
+              Icons.camera_alt,
+              color: Colors.white,
+            ),
+          ),
+          Expanded(
+              child: TabBarButton(
+            text: "CHATS",
+            textColor: index == 1 ? textIconColor : textIconColorGray,
+            borderColor: index == 1 ? textIconColor : Colors.transparent,
+          )),
+          Expanded(
+            child: TabBarButton(
+              text: "STATUS",
+              textColor: index == 2 ? textIconColor : textIconColorGray,
+              borderColor: index == 2 ? textIconColor : Colors.transparent,
+            ),
+          ),
+          Expanded(
+            child: TabBarButton(
+              text: "CALLS",
+              textColor: (index == 3 ? textIconColor : textIconColorGray),
+              borderColor: index == 3 ? textIconColor : Colors.transparent,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
 
-class barButton extends StatelessWidget {
+class TabBarButton extends StatelessWidget {
   final String text;
   final Color borderColor;
-  final Color textColor;
-  final double borderWidth;
+  final Color? textColor;
+  final double borderWidth = 3.0;
 
-  const barButton(
-      {Key? key,
-      required this.text,
-      required this.borderColor,
-      required this.textColor,
-      required this.borderWidth})
-      : super(key: key);
+  TabBarButton({
+    required this.text,
+    required this.borderColor,
+    this.textColor,
+  });
 
   @override
-  Widget build(BuildContext build) {
+  Widget build(BuildContext context) {
     return Container(
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-            border: Border(
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        border: Border(
           bottom: BorderSide(color: borderColor, width: borderWidth),
-        )),
-        child: Text(text,
-            style: TextStyle(
-                fontSize: 16, fontWeight: FontWeight.w500, color: textColor)));
+        ),
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+          color: textColor,
+        ),
+      ),
+    );
   }
 }
