7e36f2cbd500e662654fc5ef0cb2b03b1107a594
i-zanis <ioanlafazanis@gmail.com>
2021-10-31 01:30:51 +0100

    feat(pages): display usernames (test)

--- flutter/lib/presentation/pages/single_item_chat_page.dart
+++ flutter/lib/presentation/pages/single_item_chat_page.dart
@@ -8,14 +8,45 @@ class SingleItemChatPage extends StatelessWidget {
       children: <Widget>[
         Row(children: <Widget>[
           Container(
-              height: 55,
-              width: 55,
-              // rounded Rectangle
-              child: ClipRRect(
-                  borderRadius: BorderRadius.all(Radius.circular(25)),
-                  child: Image.asset("assets/profile-default.png")))
+            height: 55,
+            width: 55,
+            // rounded Rectangle
+            child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(25)),
+                child: Image.asset("assets/profile-default.png")),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Column(children: <Widget>[
+            Text(getRandomName()),
+            SizedBox(height: 5),
+          ])
         ])
       ],
     ));
+  }
+
+  // random usernames
+  static String getRandomName() {
+    List<String> names = [
+      "Baklava-kun",
+      "XiaomiBoy",
+      "MadCarlo",
+      "Doggie-chan",
+      "Chiqita",
+      "Umairdro",
+      "Marlboro",
+      "Enissay-sama",
+      "Sinnaoi",
+      "Malbec",
+      "Articuna",
+      "Zapata",
+      "Pikaru",
+      "76chan",
+      "FrenchTunisLady"
+    ];
+    names.shuffle();
+    return names.removeLast();
   }
 }
