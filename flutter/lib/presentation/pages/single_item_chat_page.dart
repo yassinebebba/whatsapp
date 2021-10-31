25b97d597933a81d4ad5da9e497fffe96737d728
i-zanis <ioanlafazanis@gmail.com>
2021-10-31 15:52:35 +0000

    feat(pages): full display user icon|message|date

--- flutter/lib/presentation/pages/single_item_chat_page.dart
+++ flutter/lib/presentation/pages/single_item_chat_page.dart
@@ -1,30 +1,58 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SingleItemChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+        margin: const EdgeInsets.only(top: 10, right: 10, left: 10),
         child: Column(
-      children: <Widget>[
-        Row(children: <Widget>[
-          Container(
-            height: 55,
-            width: 55,
-            // rounded Rectangle
-            child: ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(25)),
-                child: Image.asset("assets/profile-default.png")),
-          ),
-          SizedBox(
-            width: 10,
-          ),
-          Column(children: <Widget>[
-            Text(getRandomName()),
-            SizedBox(height: 5),
-          ])
-        ])
-      ],
-    ));
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(children: <Widget>[
+                  Container(
+                    height: 55,
+                    width: 55,
+                    // rounded Rectangle
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(25)),
+                        child: Image.asset("assets/profile-default.png")),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          getRandomName(),
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          getRandomMessage(),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        )
+                      ])
+                ]),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 60, right: 10),
+              // line between users
+              child: Divider(
+                thickness: 1.50,
+              ),
+            ),
+            Text(getRandomTime().toString())
+          ],
+        ));
   }
 
   // random usernames
@@ -41,7 +69,7 @@ class SingleItemChatPage extends StatelessWidget {
       "Sinnaoi",
       "Malbec",
       "Articuna",
-      "Zapata",
+      "Isma-akin",
       "Pikaru",
       "76chan",
       "FrenchTunisLady"
