f7087a65befd78819ee4001910230573941c35c4
i-zanis <ioanlafazanis@gmail.com>
2021-11-04 08:15:39 +0000

    feat(widgets): customActionButton for StatusPage

--- flutter/lib/presentation/pages/status_page.dart
+++ flutter/lib/presentation/pages/status_page.dart
@@ -1,3 +1,4 @@
+ 'package:cross_platform/presentation/widgets/theme/flutter_style.dart';
 import 'package:flutter/material.dart';
 
 class StatusPage extends StatelessWidget {
@@ -5,6 +6,43 @@ class StatusPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("Status Page")));
+    return Scaffold(
+        body: Scaffold(body: Stack(children: <Widget>[_customActionButton()])));
   }
+}
+
+Widget _customActionButton() {
+  return Positioned(
+      right: 10,
+      bottom: 15,
+      child: Column(children: <Widget>[
+        Container(
+          height: 45,
+          width: 45,
+          decoration: BoxDecoration(color: Colors.grey[200], boxShadow: [
+            BoxShadow(
+                offset: Offset(0, 4.0),
+                blurRadius: 0.50,
+                color: Colors.black.withOpacity(.2),
+                spreadRadius: 0.10)
+          ]),
+          child: Icon(Icons.edit, color: Colors.blueGrey),
+        ),
+        SizedBox(height: 8.0),
+        Container(
+          height: 55,
+          width: 55,
+          decoration: BoxDecoration(
+              color: primaryColor,
+              borderRadius: BorderRadius.all(Radius.circular(50)),
+              boxShadow: [
+                BoxShadow(
+                    offset: Offset(0, 4.0),
+                    blurRadius: 0.50,
+                    color: Colors.black.withOpacity(.2),
+                    spreadRadius: 0.10)
+              ]),
+          child: Icon(Icons.camera_alt, color: Colors.white),
+        )
+      ]));
 }
