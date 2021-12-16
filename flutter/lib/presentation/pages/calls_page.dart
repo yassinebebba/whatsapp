26999246ff469db9b5d4ebe6e9462174aa0ddc5e
i-zanis <ioanlafazanis@gmail.com>
2021-12-16 00:54:05 +0000

    fix(pages): ListView wrong parameters

--- flutter/lib/presentation/pages/calls_page.dart
+++ flutter/lib/presentation/pages/calls_page.dart
@@ -1,3 +1,4 @@
+import 'package:cross_platform/presentation/pages/single_item_call_page.dart';
 import 'package:cross_platform/presentation/widgets/theme/flutter_style.dart';
 import 'package:flutter/material.dart';
 
@@ -7,14 +8,15 @@ class CallsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // body: ListView.builder(itemBuilder: itemCount: 10, itemBuilder:
-      //     (BuildContext context, int index) {
-      //   return SingleItemCallPage();
-      // }),
+      body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int index) {
+            return SingleItemCallPage();
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         backgroundColor: primaryColor,
-        child: Icon(
+        child: const Icon(
           Icons.add_call,
           color: Colors.white,
         ),
