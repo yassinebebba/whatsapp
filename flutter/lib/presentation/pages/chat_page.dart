48f3d08c965b84380e08c3a86f92126c2d6ab8c9
i-zanis <ioanlafazanis@gmail.com>
2021-12-20 22:46:44 +0000

    refactor(pages): add semi-transparent icon on ChatPage

--- flutter/lib/presentation/pages/chat_page.dart
+++ flutter/lib/presentation/pages/chat_page.dart
@@ -1,4 +1,3 @@
-import 'package:cross_platform/presentation/pages/single_item_chat_page.dart';
 import 'package:cross_platform/presentation/widgets/theme/flutter_style.dart';
 import 'package:flutter/material.dart';
 
@@ -9,21 +8,34 @@ class ChatPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Expanded(
-            child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (_, index) {
-                return SingleItemChatPage();
-              },
+          Container(
+            height: 150,
+            width: 150,
+            decoration: BoxDecoration(
+                color: greenColor.withOpacity(.5),
+                borderRadius: BorderRadius.all(Radius.circular(100))),
+            child: Icon(
+              Icons.message,
+              color: Colors.white.withOpacity(.6),
+              size: 40,
             ),
           ),
+          Padding(
+              padding: const EdgeInsets.all(15),
+              child: Text(
+                  "Start chat with your friends and family,\n on "
+                  "WhatsApp.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 15, color: Colors.black.withOpacity(.4))))
         ],
       ),
       floatingActionButton: FloatingActionButton(
           backgroundColor: primaryColor,
           onPressed: () {},
-          child: Icon(Icons.chat)),
+          child: const Icon(Icons.chat)),
     );
   }
 }
