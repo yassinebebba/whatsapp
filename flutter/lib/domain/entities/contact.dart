8b5e9493a33e1d687476a433724f44d2c7313359
i-zanis <ioanlafazanis@gmail.com>
2021-12-21 15:23:30 +0000

    feat(domain): ContactEntity

---
+++ flutter/lib/domain/entities/contact.dart
@@ -0,0 +1,13 @@
+class ContactEntity {
+  final String phoneNumber;
+  final String label;
+  final String uId;
+  final String status;
+
+  ContactEntity({
+    required this.phoneNumber,
+    required this.label,
+    required this.uId,
+    required this.status,
+  });
+}
