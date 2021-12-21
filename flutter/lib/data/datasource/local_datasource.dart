93fe92c79072e5c94ab87b2d6d936599ea917546
i-zanis <ioanlafazanis@gmail.com>
2021-12-21 15:28:50 +0000

    refactor(data): add local datasource

---
+++ flutter/lib/data/datasource/local_datasource.dart
@@ -0,0 +1,21 @@
+abstract class LocalDataSource {
+  Future<List<ContactEntity>> getDeviceNumbers();
+}
+
+class LocalDataSourceImpl implements LocalDataSource {
+  @override
+  Future<List<ContactEntity>> getDeviceNumbers() async {
+    List<ContactEntity> contacts = [];
+    final getContactsData = await ContactsService.getContacts();
+
+    getContactsData.forEach((myContact) {
+      myContact.phones.forEach((phoneData) {
+        contacts.add(ContactEntity(
+          phoneNumber: phoneData.value,
+          label: myContact.displayName,
+        ));
+      });
+    });
+    return contacts;
+  }
+}
