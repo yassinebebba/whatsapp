81123f7006c6f53ce9a2715bef8e34b12bae8186
i-zanis <ioanlafazanis@gmail.com>
2021-12-22 23:27:37 +0000

    refactor(pages): route to SelectContactPage

--- flutter/lib/presentation/pages/chat_page.dart
+++ flutter/lib/presentation/pages/chat_page.dart
@@ -1,3 +1,5 @@
+import 'package:cross_platform/domain/entities/user.dart';
+import 'package:cross_platform/presentation/pages/select_contact_page.dart';
 import 'package:cross_platform/presentation/widgets/theme/flutter_style.dart';
 import 'package:flutter/material.dart';
 
@@ -14,28 +16,47 @@ class ChatPage extends StatelessWidget {
             height: 150,
             width: 150,
             decoration: BoxDecoration(
-                color: greenColor.withOpacity(.5),
-                borderRadius: BorderRadius.all(Radius.circular(100))),
+              color: greenColor.withOpacity(.5),
+              borderRadius: BorderRadius.all(Radius.circular(100)),
+            ),
             child: Icon(
               Icons.message,
               color: Colors.white.withOpacity(.6),
               size: 40,
             ),
           ),
-          Padding(
+          Align(
+            child: Padding(
               padding: const EdgeInsets.all(15),
               child: Text(
-                  "Start chat with your friends and family,\n on "
-                  "WhatsApp.",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 15, color: Colors.black.withOpacity(.4))))
+                "Start chat with your friends and family,\n on WhatsApp Clone",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 14, color: Colors.black.withOpacity(.4)),
+              ),
+            ),
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-          backgroundColor: primaryColor,
-          onPressed: () {},
-          child: const Icon(Icons.chat)),
+        backgroundColor: primaryColor,
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (_) => SelectContactPage(
+                        userInfo: UserEntity(
+                            name: "Name",
+                            email: "email@",
+                            phoneNumber: "11515",
+                            isOnline: false,
+                            uId: '1515',
+                            profileUrl: 'google.com',
+                            statusId: '555'),
+                      )));
+        },
+        child: Icon(Icons.chat),
+      ),
     );
   }
 }
